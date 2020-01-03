9339ddac94055ecd2af37cf4d53da3b6d86910f5
Walter Mitty <gsitcia@gmail.com>
2020-01-02 21:50:30 -0600

    Initial Commit

---
+++ src/main/java/com/gsitcia/adwellia/Adwellia.java
@@ -0,0 +1,10 @@
+package com.gsitcia.adwellia;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Adwellia implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("ANAGRAMS");
+	}
+}
